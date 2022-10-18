ad75d759109dec0095deee47750a4d74e7e73774
Adriana Le Compte-Santiago <adrianals@sccwrp.org>
2022-10-18 15:49:50 -0700

    finalized sample ID, created P_A matrix

--- nMDR.R
+++ nMDR.R
@@ -8,8 +8,23 @@ library(readxl)
 benthic_all_df <- read_excel("BBTrib_Benthic_2018_2019.xlsx") %>% as_tibble
 
 benthic_df <- select(benthic_all_df, StationCode, SampleDate, CollectionMethodName, FinalID, BAResult, CollectionReplicate) %>% 
-  filter(CollectionMethodName == "BMI_Reach-WideBenthos")
+  filter(CollectionMethodName == "BMI_Reach-WideBenthos") %>% 
+  mutate(CollectionMethod = "BMI_RWB")
 
-benthic_df_ID <- benthic_df_BMI %>% 
-  unite("SampleID", StationCode, SampleDate, CollectionReplicate, remove = FALSE)
+benthic_df_ID <- benthic_df %>% 
+  unite("SampleID", StationCode, SampleDate, CollectionMethod, CollectionReplicate) %>% 
+  mutate(P_A = 1) %>% 
+  select(-c(CollectionMethodName, BAResult)) %>% 
+  group_by(SampleID, FinalID) %>% 
+  summarize(P_A = sum(P_A)) %>% 
+  ungroup() %>% 
+  mutate(P_A = case_when(P_A >= 1 ~ 1,
+                   TRUE ~ 0))
+
+unique(benthic_df_ID$FinalID) #146 species
+unique(benthic_df_ID$SampleID) #24 unique sample IDs
+
+benthic_df_ID %>% pivot_wider(names_from = FinalID, values_from = P_A, values_fill = 0) %>% View()
+
+benthic_df_ID %>% select(-P_A) %>% duplicated()
 
