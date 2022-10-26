2ef65129c9e1ca6fe5a9182887671385a8f154f6
Adriana Le Compte-Santiago <adrianals@sccwrp.org>
2022-10-26 12:04:41 -0700

    create p_a and abun matrices correctly

--- nMDR.R
+++ nMDR.R
@@ -11,35 +11,40 @@ benthic_all_df <- read_excel("BBTrib_Benthic_2018_2019.xlsx") %>% as_tibble
 benthic_df <- benthic_all_df %>% filter(CollectionMethodName == "BMI_Reach-WideBenthos") %>% 
   mutate(CollectionMethod = "BMI_RWB", SampleID=paste(StationCode, SampleDate, CollectionMethod, CollectionReplicate, sep="_"))
 
-#create dataframe as p_a matrix (24 unique sampleIDs, 146 unique species)
-benthic_df_pa <- benthic_df %>% 
-  unite("SampleID", StationCode, SampleDate, CollectionMethod, CollectionReplicate) %>%
-  mutate(p_a = 1) %>% #create sampleID
-  group_by(SampleID, FinalID) %>% #group the presence by sample ID and final ID
-  summarize(p_a = sum(p_a)) %>% #summarize the presence by sample ID and final ID
+#create dataframe for abundance matrix
+benthic_df_abun <- benthic_df %>% 
+  group_by(SampleID, FinalID) %>% 
+  summarize(BAResult_sum = sum(BAResult)) %>% 
   ungroup() %>% 
-  pivot_wider(names_from = FinalID, values_from = p_a, values_fill = 0) %>% #pivot dataframe
-  column_to_rownames("SampleID") #move SampleID column into rownames
+  pivot_wider(names_from = FinalID, values_from = BAResult_sum, values_fill = 0) %>% 
+  column_to_rownames("SampleID")
 
-#create numeric matrix from dataframe
-ben_matrix <- data.matrix(benthic_df_ID)
+#create dataframe for p_a matrix
+benthic_df_pa <- benthic_df %>% 
+  group_by(SampleID, FinalID) %>% 
+  summarize(BAResult_sum = sum(BAResult)) %>% 
+  ungroup() %>% 
+  mutate(p_a = 1) %>%
+  select(-BAResult_sum) %>% 
+  pivot_wider(names_from = FinalID, values_from = p_a, values_fill = 0) %>% 
+  column_to_rownames("SampleID")
 
 #run nMDS function to get ordination object
-benthic_ord <- metaMDS(ben_matrix, k = 2, try = 20, trymax = 50, autotransform = F)
+benthic_ord_pa <- metaMDS(benthic_df_pa, k = 2, try = 20, trymax = 50, autotransform = F)
 
 #turn object into dataframe to extract scores
-benthic_samples <- data.frame(scores(benthic_ord, display = "sites")) %>% 
-  add_column(.before = "NMDS1", benthic_df_pa$SampleID)
+benthic_samples_pa <- data.frame(scores(benthic_ord_pa, display = "sites"))
+  #add_column(.before = "NMDS1", benthic_df_pa$SampleID)
 
 #create csv with results
-write.csv(benthic_samples, file = "nMDR results.csv", row.names = FALSE)
+#write.csv(benthic_samples, file = "nMDR results.csv", row.names = FALSE)
 
 #calculate other metrics
 
 #richness
-richness_col <- benthic_df %>%
-  group_by(SampleID, FinalID) %>% 
-  summarize(BAResult)
+#richness_col <- benthic_df %>%
+  #group_by(SampleID, FinalID) %>% 
+  #summarize(BAResult)
 
 #evenness
-diversity(benthic_df)
+#diversity(benthic_df)
