d69be0f3cbee1171581d24385c6e1e6f7ab8b9de
Adriana Le Compte-Santiago <adrianals@sccwrp.org>
2022-10-27 13:44:29 -0700

    create univariate script

---
+++ univariate_results.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(vegan)
+library(ggplot2)
+library(readxl)
+
+benthic_all_df <- read_excel("BBTrib_Benthic_2018_2019.xlsx") %>% as_tibble
+
+#select columns of interest
+benthic_df <- benthic_all_df %>% filter(CollectionMethodName == "BMI_Reach-WideBenthos") %>% 
+  mutate(CollectionMethod = "BMI_RWB", SampleID=paste(StationCode, SampleDate, CollectionMethod, CollectionReplicate, sep="_"))
+
+#create dataframe for abundance matrix
+benthic_df_abun <- benthic_df %>% 
+  group_by(SampleID, FinalID) %>% 
+  summarize(BAResult_sum = sum(BAResult)) %>% 
+  ungroup() %>% 
+  pivot_wider(names_from = FinalID, values_from = BAResult_sum, values_fill = 0)
+
+diver_results <- benthic_df_abun %>% mutate(H = diversity(select(benthic_df_abun, 2:147), index = "shannon"))
+
+univ_results <- benthic_df %>% 
+  group_by(SampleID) %>% 
+  summarise(S = length(FinalID)) %>% 
+  ungroup() %>% 
+  left_join(., (select(diver_results, SampleID, H)), by = "SampleID")
