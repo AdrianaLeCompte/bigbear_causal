6affe3301fd5e46f7231cff6169e0855dbb8298d
Adriana Le Compte-Santiago <adrianals@sccwrp.org>
2022-10-27 13:43:36 -0700

    plot abun, p_a scores correctly

--- bigbear 18-19 ordination outputs.R
+++ bigbear 18-19 ordination outputs.R
@@ -4,6 +4,9 @@ library(vegan)
 library(ggplot2)
 library(readxl)
 
+install.packages("paletteer")
+library(paletteer)
+
 #pull in data
 benthic_all_df <- read_excel("BBTrib_Benthic_2018_2019.xlsx") %>% as_tibble
 
@@ -32,14 +35,51 @@ benthic_df_pa <- benthic_df %>%
 #run nMDS function to get ordination object
 benthic_ord_pa <- metaMDS(benthic_df_pa, k = 2, try = 20, trymax = 50, autotransform = F)
 
+stress_pa = benthic_ord_pa$stress
+
+benthic_ord_abun <- metaMDS(benthic_df_abun, k = 2, try = 20, trymax = 50, autotransform = F)
+
+stress_abun = benthic_ord_abun$stress
+
 #extract scores from ordination
 benthic_scores_pa <- data.frame(scores(benthic_ord_pa, display = "sites")) %>% 
   rownames_to_column("SampleID") %>% 
   mutate(StationID = str_sub(SampleID,1,9), SamDate = str_sub(SampleID,11,20), 
          Replicate = str_sub(SampleID, -1), SamDate = lubridate::as_date(SamDate),
-         Year = lubridate::year(SamDate), Month = lubridate::month(SamDate, label=TRUE, abbr = F))
+         Year = lubridate::year(SamDate), Month = lubridate::month(SamDate, label=TRUE, abbr = F)) %>% 
+  mutate(Year = as.factor(Year))
+
+benthic_scores_abun <- data.frame(scores(benthic_ord_abun, display = "sites")) %>% 
+  rownames_to_column("SampleID") %>% 
+  mutate(StationID = str_sub(SampleID,1,9), SamDate = str_sub(SampleID,11,20), 
+         Replicate = str_sub(SampleID, -1), SamDate = lubridate::as_date(SamDate),
+         Year = lubridate::year(SamDate), Month = lubridate::month(SamDate, label=TRUE, abbr = F)) %>% 
+  mutate(Year = as.factor(Year))
 
 #plot ordination scores
-ggplot(benthic_scores_pa, aes(x = NMDS1, y = NMDS2, colour = StationID)) +
-  geom_point()
-  #geom_label(aes(label = Month))
+bb.pa.plot <- benthic_scores_pa %>% 
+  ggplot(., aes(x = NMDS1, y = NMDS2)) +
+  theme_bw()+ theme(panel.grid = element_blank(), axis.text = element_blank(), axis.ticks = element_blank(),
+                    axis.title = element_text(face = "bold"))+
+  geom_point(aes(fill = StationID, shape = Year), size = 2.5)+
+  scale_shape_manual(values = c(21,22))+
+  geom_text(aes(label = Month), nudge_y = -0.025)+
+  guides(fill=guide_legend(override.aes = list(shape=21)))+
+  labs(title = 'Presence',
+       subtitle = 'Big bear causal assessment 2018-2019'
+         )
+bb.pa.plot
+
+bb.pa.plot2 <- benthic_scores_abun %>% 
+  ggplot(., aes(x = NMDS1, y = NMDS2)) +
+  theme_bw()+ theme(panel.grid = element_blank(), axis.text = element_blank(), axis.ticks = element_blank(),
+                    axis.title = element_text(face = "bold"))+
+  geom_point(aes(fill = StationID, shape = Year), size = 2.5)+
+  scale_shape_manual(values = c(21,22))+
+  geom_text(aes(label = Month), nudge_y = -0.025)+
+  guides(fill=guide_legend(override.aes = list(shape=21)))+
+  labs(title = 'Abundance',
+       subtitle = 'Big bear causal assessment 2018-2019'
+  )
+bb.pa.plot2
+
