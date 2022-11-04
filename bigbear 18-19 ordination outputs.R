d179883a21b17e07e78a821cc98207df6b27c371
Adriana Le Compte-Santiago <adrianals@sccwrp.org>
2022-11-03 17:27:35 -0700

    calculate ordinations by year

--- bigbear 18-19 ordination outputs.R
+++ bigbear 18-19 ordination outputs.R
@@ -12,6 +12,10 @@ benthic_all_df <- read_excel("BBTrib_Benthic_2018_2019.xlsx") %>% as_tibble
 benthic_df <- benthic_all_df %>% filter(CollectionMethodName == "BMI_Reach-WideBenthos") %>% 
   mutate(CollectionMethod = "BMI_RWB", SampleID=paste(StationCode, SampleDate, CollectionMethod, CollectionReplicate, sep="_"))
 
+
+# Ordination with both years ----------------------------------------------
+
+
 #create dataframe for abundance matrix
 benthic_df_abun <- benthic_df %>% 
   group_by(SampleID, FinalID) %>% 
@@ -84,5 +88,73 @@ bb.pa.plot2
 ggsave("bigbear nMDS ord abundance.png", bb.pa.plot2)
 ggsave("bigbear nMDS ord presence.png", bb.pa.plot)
 
-install.packages("ggrepel")
 
+
+# Abundance ordination by year --------------------------------------------
+
+#create dataframe for abundance matrix 
+benthic_df_abun_18 <- benthic_df %>%
+  mutate(Year = lubridate::year(SampleDate))%>% 
+  filter(Year == "2018") %>% 
+  group_by(SampleID, FinalID) %>% 
+  summarize(BAResult_sum = sum(BAResult)) %>% 
+  ungroup() %>% 
+  pivot_wider(names_from = FinalID, values_from = BAResult_sum, values_fill = 0) %>% 
+  column_to_rownames("SampleID")
+
+benthic_df_abun_19 <- benthic_df %>%
+  mutate(Year = lubridate::year(SampleDate))%>% 
+  filter(Year == "2019") %>% 
+  group_by(SampleID, FinalID) %>% 
+  summarize(BAResult_sum = sum(BAResult)) %>% 
+  ungroup() %>% 
+  pivot_wider(names_from = FinalID, values_from = BAResult_sum, values_fill = 0) %>% 
+  column_to_rownames("SampleID")
+
+#run nMDS function to get ordination object & stress
+benthic_ord_abun_18 <- metaMDS(benthic_df_abun_18, k = 2, try = 20, trymax = 50, autotransform = F)
+stress_abun_18 = benthic_ord_abun_18$stress
+
+benthic_ord_abun_19 <- metaMDS(benthic_df_abun_19, k = 2, try = 20, trymax = 50, autotransform = F)
+stress_abun_19 = benthic_ord_abun_19$stress
+
+#extract scores from ordination
+benthic_scores_abun_18 <- data.frame(scores(benthic_ord_abun_18, display = "sites")) %>% 
+  rownames_to_column("SampleID") %>% 
+  mutate(StationID = str_sub(SampleID,1,9), SamDate = str_sub(SampleID,11,20), 
+         Replicate = str_sub(SampleID, -1), SamDate = lubridate::as_date(SamDate),
+         Year = lubridate::year(SamDate), Month = lubridate::month(SamDate, label=TRUE, abbr = F)) %>% 
+  mutate(Year = as.factor(Year))
+
+benthic_scores_abun_19 <- data.frame(scores(benthic_ord_abun_19, display = "sites")) %>% 
+  rownames_to_column("SampleID") %>% 
+  mutate(StationID = str_sub(SampleID,1,9), SamDate = str_sub(SampleID,11,20), 
+         Replicate = str_sub(SampleID, -1), SamDate = lubridate::as_date(SamDate),
+         Year = lubridate::year(SamDate), Month = lubridate::month(SamDate, label=TRUE, abbr = F)) %>% 
+  mutate(Year = as.factor(Year))
+
+#plot ordination scores
+bb.abun.plot.18 <- benthic_scores_abun_18 %>% 
+  ggplot(., aes(x = NMDS1, y = NMDS2)) +
+  theme_bw()+ theme(panel.grid = element_blank(), axis.text = element_blank(), axis.ticks = element_blank(),
+                    axis.title = element_text(face = "bold"))+
+  geom_point(aes(fill = StationID), pch = 21, size = 2.5)+
+  geom_text_repel(aes(NMDS1, NMDS2, label = Month))+
+  labs(title = 'Abundance',
+       subtitle = 'Big bear causal assessment 2018'
+  )
+bb.abun.plot.18
+
+bb.abun.plot.19 <- benthic_scores_abun_19 %>% 
+  ggplot(., aes(x = NMDS1, y = NMDS2)) +
+  theme_bw()+ theme(panel.grid = element_blank(), axis.text = element_blank(), axis.ticks = element_blank(),
+                    axis.title = element_text(face = "bold"))+
+  geom_point(aes(fill = StationID), pch = 21, size = 2.5)+
+  geom_text_repel(aes(NMDS1, NMDS2, label = Month))+
+  labs(title = 'Abundance',
+       subtitle = 'Big bear causal assessment 2019'
+  )
+bb.abun.plot.19
+
+
+# Pres/Abs ordination by year ---------------------------------------------
