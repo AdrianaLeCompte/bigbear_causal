11a0ef5ef040bd07ac06def4f5f4c7cdcb15fe4b
Adriana Le Compte-Santiago <adrianals@sccwrp.org>
2022-11-04 12:05:46 -0700

    Added pres-abs calculations by year

--- bigbear 18-19 ordination outputs.R
+++ bigbear 18-19 ordination outputs.R
@@ -165,3 +165,78 @@ ggsave("results/bigbear nMDS ord abun 18.png", bb.abun.plot.18)
 ggsave("results/bigbear nMDS ord abun 19.png", bb.abun.plot.19)
 
 # Pres/Abs ordination by year ---------------------------------------------
+
+#create dataframe for p_a matrix
+benthic_df_pa_18 <- benthic_df %>% 
+  mutate(Year = lubridate::year(SampleDate))%>% 
+  filter(Year == "2018") %>%
+  group_by(SampleID, FinalID) %>% 
+  summarize(BAResult_sum = sum(BAResult)) %>% 
+  ungroup() %>% 
+  mutate(p_a = 1) %>%
+  select(-BAResult_sum) %>% 
+  pivot_wider(names_from = FinalID, values_from = p_a, values_fill = 0) %>% 
+  column_to_rownames("SampleID")
+
+benthic_df_pa_19 <- benthic_df %>% 
+  mutate(Year = lubridate::year(SampleDate))%>% 
+  filter(Year == "2019") %>%
+  group_by(SampleID, FinalID) %>% 
+  summarize(BAResult_sum = sum(BAResult)) %>% 
+  ungroup() %>% 
+  mutate(p_a = 1) %>%
+  select(-BAResult_sum) %>% 
+  pivot_wider(names_from = FinalID, values_from = p_a, values_fill = 0) %>% 
+  column_to_rownames("SampleID")
+
+#run nMDS function to get ordination object
+benthic_ord_pa_18 <- metaMDS(benthic_df_pa_18, k = 2, try = 1000, trymax = 10000, autotransform = F)
+stress_pa_18 = benthic_ord_pa_18$stress
+
+benthic_ord_pa_19 <- metaMDS(benthic_df_pa_19, k = 2, try = 1000, trymax = 10000, autotransform = F)
+stress_pa_19 = benthic_ord_pa_19$stress
+
+#extract scores from ordination
+benthic_scores_pa_18 <- data.frame(scores(benthic_ord_pa_18, display = "sites")) %>% 
+  rownames_to_column("SampleID") %>% 
+  mutate(StationCode = str_sub(SampleID,1,9), SamDate = str_sub(SampleID,11,20), 
+         Replicate = str_sub(SampleID, -1), SamDate = lubridate::as_date(SamDate),
+         Year = lubridate::year(SamDate), Month = lubridate::month(SamDate, label=TRUE, abbr = F)) %>% 
+  mutate(Year = as.factor(Year)) %>%  
+  left_join(x = {.}, y = short_names, by = "StationCode")
+
+benthic_scores_pa_19 <- data.frame(scores(benthic_ord_pa_19, display = "sites")) %>% 
+  rownames_to_column("SampleID") %>% 
+  mutate(StationCode = str_sub(SampleID,1,9), SamDate = str_sub(SampleID,11,20), 
+         Replicate = str_sub(SampleID, -1), SamDate = lubridate::as_date(SamDate),
+         Year = lubridate::year(SamDate), Month = lubridate::month(SamDate, label=TRUE, abbr = F)) %>% 
+  mutate(Year = as.factor(Year)) %>%  
+  left_join(x = {.}, y = short_names, by = "StationCode")
+
+#plot ordination scores
+bb.pa.plot.18 <- benthic_scores_pa_18 %>% 
+  ggplot(., aes(x = NMDS1, y = NMDS2)) +
+  theme_bw()+ theme(panel.grid = element_blank(), axis.text = element_blank(), axis.ticks = element_blank(),
+                    axis.title = element_text(face = "bold"))+
+  geom_point(aes(fill = SimpleName), pch = 21, size = 2.5)+
+  scale_fill_discrete(name = "Station Name")+
+  geom_text_repel(aes(NMDS1, NMDS2, label = Month))+
+  labs(title = 'Presence Absence',
+       subtitle = 'Big bear causal assessment 2018')
+bb.pa.plot.18
+
+bb.pa.plot.19 <- benthic_scores_pa_19 %>% 
+  ggplot(., aes(x = NMDS1, y = NMDS2)) +
+  theme_bw()+ theme(panel.grid = element_blank(), axis.text = element_blank(), axis.ticks = element_blank(),
+                    axis.title = element_text(face = "bold"))+
+  geom_point(aes(fill = SimpleName), pch = 21, size = 2.5)+
+  scale_fill_discrete(name = "Station Name")+
+  geom_text_repel(aes(NMDS1, NMDS2, label = Month))+
+  labs(title = 'Presence Absence',
+       subtitle = 'Big bear causal assessment 2019')
+bb.pa.plot.19
+
+#save graphs
+ggsave("results/bigbear nMDS ord pa 18.png", bb.pa.plot.18)
+ggsave("results/bigbear nMDS ord pa 19.png", bb.pa.plot.19)
+
